811f86a1c666f8cc798ad54374440c3fbc420a5d
Ahsan <ahsanzulfiqar123786@gmail.com>
2023-10-06 15:41:34 +0500

    intall express

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 import fs from "fs";
 import http from "http";
+import express from "express"
 
 const fileData = fs.readFileSync("message.txt", "utf-8");
 const productData = fs.readFileSync("productsData.json", "utf-8");
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/":
-      res.writeHead(200, { "content-type": "text/html" });
-      res.end(fileData);
-      break;
 
-    case "/products":
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(productData);
-      break;
-    default:
-      res.writeHead(404, { "content-type": "text/html" });
-      res.end("<h1>404 page not found</h1>");
-      break;
-  }
-});
+const server = express();
 
-server.listen(8080);
+server.get("/", (req , res)=>{
+    res.header("name","ahsan").send("<h1>Get Request</h1>");
+})
+server.put("/", (req , res)=>{
+    res.header("name","ahsan").send("<h1>PUT Request</h1>");
+})
+server.post("/", (req , res)=>{
+    res.header("name","ahsan").send("<h1>POST Request</h1>");
+})
+server.delete("/", (req , res)=>{
+    res.header("name","ahsan").send("<h1>DELETE Request</h1>");
+})
+server.patch("/", (req , res)=>{
+    res.header("name","ahsan").send("<h1>PATCH' Request</h1>");
+})
+
+
+server.listen(8080, ()=>{
+    console.log("Server is running on port 8080");
+})
+
